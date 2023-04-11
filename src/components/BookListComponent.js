45d60623eeea70773490eefb889c0bb69b162ff7
deepakbarmola <deepakbarmola@gmail.com>
2023-04-11 17:13:05 +0530

    installed scss and wip

--- src/components/BookListComponent.js
+++ src/components/BookListComponent.js
@@ -1,11 +1,38 @@
 import React, {useState} from 'react';
-import LINK from 'react-router-dom';
+import BookDetailsTile from '../reusableComponents/BookDetailsTile';
 
 export default function BookListComponent(props){
 
+    const [detailOfBooks, setDetailOfBooks] = useState([]);
+
+    const x = [ 
+        {bookName: 'The alchemist', author: 'Paulo Coehlo'},
+        {bookName: 'The alchemist', author: 'Paulo Coehlo'},
+        {bookName: 'The alchemist', author: 'Paulo Coehlo'},
+        {bookName: 'The alchemist', author: 'Paulo Coehlo'},
+        {bookName: 'The alchemist', author: 'Paulo Coehlo'}
+        ]
+
     return(
         <div>
-            hey there
+        <div>
+          <span>
+            E - LIBRARY
+          </span>
+          <span>
+            <button>ADD A BOOK</button>
+          </span>
+        </div>
+        <div>
+        {x.map((detail, index)=>{
+            return(
+                 <BookDetailsTile key={index} bookName={detail.bookName} author={detail.author} />
+            );
+           
+        })
+        }
+        </div>
+
         </div>
     )
 }
---
+++ src/reusableComponents/BookDetailsTile.js
@@ -0,0 +1,19 @@
+import react from 'react';
+
+export default function BookDetailsTile(props){
+
+    return(
+        <div>
+            <div>
+                <span>
+                    BOOK'S IMAGE
+                </span>
+            </div>
+            <div>
+                <div>Book Title</div>
+                <div>Author</div>
+                <div><a>MORE INFO</a></div>
+            </div>
+        </div>
+    )
+}
